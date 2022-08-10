1da67120eb4835565ad8ae277a202cdc75d9dd75
nubisub <hendraksm18@mail.ugm.ac.id>
2022-08-10 23:43:31 +0700

    CRUD done with no validation

---
+++ README.md
@@ -0,0 +1,22 @@
+<div align="center">
+<h1>Simple CRUD Contacts App</h1>
+
+![NodeJS](https://img.shields.io/badge/node.js-6DA55F?style=for-the-badge&logo=node.js&logoColor=white)
+![Express.js](https://img.shields.io/badge/express.js-%23404d59.svg?style=for-the-badge&logo=express&logoColor=%2361DAFB)
+![TailwindCSS](https://img.shields.io/badge/tailwindcss-%2338B2AC.svg?style=for-the-badge&logo=tailwind-css&logoColor=white)
+<!-- ![MongoDB](https://img.shields.io/badge/MongoDB-%234ea94b.svg?style=for-the-badge&logo=mongodb&logoColor=white) -->
+
+<div>
+
+<div align="left">
+
+> [Reference](https://www.youtube.com/watch?v=sSLJx5t4OJ4&list=PLFIM0718LjIW-XBdVOerYgKegBtD6rSfD)
+
+<details>
+<summary></summary>
+<br>
+:middle_finger: validation rule
+</details>
+
+</div>
+
--- app.js
+++ app.js
@@ -5,13 +5,15 @@ const app = express();
 const port = 3000;
 const expressLayouts = require("express-ejs-layouts");
 app.use(expressLayouts);
+const contacts = require("./utils/contact");
 
 app.set("view engine", "ejs");
-
+const bodyParser = require("body-parser");
 // built in middleware
 // make public assets available to the app
 app.use(express.static("public"));
-
+// app.use(express.urlencoded ());
+app.use(bodyParser.urlencoded({ extended: true }));
 
 // middleware
 // app.use((req, res, next) => {
@@ -21,9 +23,7 @@ app.use(express.static("public"));
 // Routing
 
 app.get("/", (req, res) => {
-	const data = fs.readFileSync("db.json");
 	res.render("index", {
-		data: JSON.parse(data),
 		title: "Contact: Home",
 		layout: "layouts/main-layout",
 	});
@@ -38,18 +38,61 @@ app.get("/about", (req, res) => {
 
 app.get("/contact", (req, res) => {
 	res.render("contact", {
+		data: contacts.loadContacts(),
 		layout: "layouts/main-layout",
 		title: "Contact: Contact",
 	});
 });
 
-app.use("/", (req, res) => {
-	res.status(404);
-	res.render("error", {
+app.post("/contact", (req, res) => {
+	contacts.addContact(req.body);
+	res.redirect("/contact");
+});
+
+app.get("/add", (req, res) => {
+	res.render("add", {
 		layout: "layouts/main-layout",
-		title: "Contact: Error",
+		title: "Contact: Add",
+	});
+}),
+app.post("/contact/update/:id", (req, res) => {
+	contacts.updateContact(req.params.id, req.body);
+	console.log(req.body);
+	res.redirect("/contact");
+}),
+
+	app.get("/contact/delete/:id", (req, res) => {
+		const data = contacts.findContacts(req.params.id);
+		if (data) {
+			contacts.deleteContact(req.params.id);
+			res.redirect("/contact");
+		} else {
+			res.redirect("/contact");
+		}
+	});
+
+app.get("/contact/:nama", (req, res) => {
+	res.render("detail", {
+		data: contacts.findContacts(req.params.nama),
+		layout: "layouts/main-layout",
+		title: "Contact: Contact",
 	});
 });
+
+	app.get("/contact/edit/:id", (req, res) => {
+		res.render("edit", {
+			data: contacts.findContacts(req.params.id),
+			layout: "layouts/main-layout",
+			title: "Contact: Edit",
+		});
+	}),
+	app.use("/", (req, res) => {
+		res.status(404);
+		res.render("error", {
+			layout: "layouts/main-layout",
+			title: "Contact: Error",
+		});
+	});
 
 app.listen(port, () => {
 	console.log(`Server started on port ${port}`);
