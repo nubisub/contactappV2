1a48a60fa3448902d36d4c66c778445c86688ec3
nubisub <hendraksm18@mail.ugm.ac.id>
2022-08-11 13:54:18 +0700

    update port

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 5000;
 const expressLayouts = require("express-ejs-layouts");
 app.use(expressLayouts);
 require("dotenv").config();
